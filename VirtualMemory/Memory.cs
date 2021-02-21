d5dfe579c6461763bccc7e07cf638d4e26edc94b
Igor Anufriev <sacmi@pm.me>
2021-02-22 00:31:44 +0700

    каркас

---
+++ VirtualMemory/Memory.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+
+namespace VirtualMemory
+{
+    public class Memory
+    {
+        private const ushort BufferSize = 5;
+
+        private MemoryList[] buffer = new MemoryList[BufferSize];
+        
+        public Memory(ulong size, string filename = "vm.bin")
+        {
+            // тут должна быть проверка на существующую виртуальную память
+            // если ее нет, создать
+        }
+
+        public ulong GetAddress(int index)
+        {
+            // получть абсолютный путь по индексу
+            return 0;
+        }
+        
+        public MemoryList this[int index]
+        {
+            get
+            {
+                // реализовать чтение из памяти
+                return null;
+            }
+            set
+            {
+                // реализовать запись в память
+                // с проверкой на блокировку
+            }
+        }
+        
+        
+    }
+}
---
+++ VirtualMemory/MemoryList.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+
+namespace VirtualMemory
+{
+    public class MemoryList
+    {
+        private const uint Size = 512;
+
+        // модифицировалась ли страница
+        public bool Modified;
+
+        // свободна ли для записи
+        public bool Locked;
+
+        // количество обращений
+        public uint Counter;
+
+        // время записи
+        public uint Timestamp;
+
+        // номер страиницы
+        public readonly uint Id;
+
+        // значения
+        public int[] Values;
+
+        public MemoryList(uint id, Stream streamReader)
+        {
+            var binReader = new BinaryReader(streamReader);
+            Id = id;
+            Values = new int[Size / sizeof(int)];
+
+            try
+            {
+                Modified = binReader.ReadBoolean();
+                Locked = binReader.ReadBoolean();
+                Counter = (uint) binReader.ReadUInt32();
+                Timestamp = binReader.ReadUInt32();
+            }
+            catch (Exception e)
+            {
+                Modified = false;
+                Locked = false;
+                Counter = (uint) 0;
+                Timestamp = 0;
+            }
+        }
+    }
+}
